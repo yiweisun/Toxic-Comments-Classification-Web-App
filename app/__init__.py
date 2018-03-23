3df34d3fcf789d92ecb53289648f4446c46c413e
yiweisun <yiweisun2018@u.northwestern.edu>
2018-03-23 04:44:08 -0500

    change _init_.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,5 +10,3 @@ application = Flask(__name__) # Application instance
 # This 'APP_SETTINGS' is an export PATH locally 
 application.config.from_envvar('APP_SETTINGS', silent=True)
 
-# Initialize the database
-db = SQLAlchemy(application)
